56a87548e193a354f5c34ca1992aab97574cb2f3
Jean-Baptiste SALVI <jeanbaptistesalvi@hotmail.fr>
2023-04-21 14:24:00 +0200

    Architecture application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleComponent } from './title/title.component';
+import { SignupComponent } from './signup/signup.component';
+import { GalerieComponent } from './galerie/galerie.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    TitleComponent,
+    SignupComponent,
+    GalerieComponent,
+    ContactComponent,
+    ReservationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
